ff4a7cb75a9dd5079aadaf4b70b09583e1d762be
Janis Krasemann <janis.krasemann@enpit.de>
2016-11-01 17:48:09 +0100

    Add validation to the add-artist form

--- src/js/viewModels/add-artist.js
+++ src/js/viewModels/add-artist.js
@@ -10,6 +10,7 @@ define([
   'ojs/ojbutton',
   'ojs/ojinputnumber',
   'ojs/ojinputtext',
+  'ojs/ojknockout-validation',
   'ojs/ojselectcombobox'
 ], function (oj, ko, $, spotify) {
   /**
@@ -24,13 +25,53 @@ define([
 
     self.tracker = ko.observable();
 
+    /**
+     * Determines when the add-artist button will be disabled
+     *
+     * - If there are invalid components currently showing messages
+     * this method returns true and the button is disabled.
+     * - If there are no visible errors, this method returns false
+     * and the button is enabled.
+     * E.g., when there are deferred errors on the component the
+     * button is enabled.
+     *
+     * @return {boolean} forms are valid
+     */
+    self.shouldDisableAdd = function () {
+      var trackerObj = ko.utils.unwrapObservable(self.tracker);
+      var hasInvalidComponents =
+        trackerObj &&
+        (trackerObj.invalidShown || self.genre()[0] === 'default');
+
+      return hasInvalidComponents;
+    };
+
+    // Returns false if there are components showing errors.
+    // Returns true if all components are valid.
+    var isValid = function isValid (trackerObj) {
+      trackerObj.showMessages();
+      return !trackerObj.focusOnFirstInvalid();
+    };
+
+    /**
+     * Click handler that validates form.
+     *
+     * If there are deferred errors they are displayed
+     * to the end-user by calling the
+     * InvalidComponentTracker#showMessages method.
+     * Then focus is set on the first invalid component -
+     * InvalidComponentTracker#focusOnFirstInvalid and method returns.
+     */
     self.save = function () {
-      ko.postbox.publish('add-artist', {
-        name: self.name(),
-        genre: self.genre()[0],
-        year: self.year()
-      });
-      window.history.back();
+      var trackerObj = ko.utils.unwrapObservable(self.tracker);
+      if (isValid(trackerObj)) {
+        ko.postbox.publish('add-artist', {
+          name: self.name(),
+          genre: self.genre()[0],
+          year: self.year()
+        });
+        window.history.back();
+      }
     };
   }
   return AddArtistViewModel;
